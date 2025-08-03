d65c5f9e82b6869995b2a6a3a2eb80728c7d152f
codezella-hub <benslimenlouay29@gmail.com>
2025-08-03 05:24:59 +0200

    update

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+// hello 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
